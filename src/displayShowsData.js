e02806478bc61306d693100a1755017393b7d736
John Murianki <mcjthiongo@gmail.com>
2023-03-28 22:42:50 +0300

    Add homepage

--- src/displayShowsData.js
+++ src/displayShowsData.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-console */
+// mport './styles.css';
 import fetchShowsData from './fetchShowsData.js';
 
 const display = document.querySelector('.display');
-display.innerHTML = 'Loading...';
+display.innerHTML = '';
 
 // Displays the TV show data in the HTML page
 export default function displayShowsData() {
@@ -12,21 +13,59 @@ export default function displayShowsData() {
       console.log(showDataList);
 
       // Build the HTML string to display the TV show information
-      let html = '';
       showDataList.forEach((showData) => {
-        html += '<div>';
-        html += `<h2>${showData.name}</h2>`;
-        html += `<img src="${showData.image.medium}" alt="${showData.name}">`;
-        html += `<p>${showData.summary}</p>`;
-        html += '<div class="button-container">';
-        html += '<button class="reservations-button">Reservations</button>';
-        html += '<button class="lie-button">Lies</button>';
-        html += '</div>';
-        html += '</div>';
-      });
+        const showCard = document.createElement('div');
+        showCard.classList.add('show-card');
 
-      // Set the innerHTML of the display element to the HTML string
-      display.innerHTML = html;
+        const showCardHeader = document.createElement('div');
+        showCardHeader.classList.add('show-card-header');
+
+        const showCardTitle = document.createElement('h2');
+        showCardTitle.classList.add('show-card-title');
+        showCardTitle.innerText = showData.name;
+
+        const showCardButtons = document.createElement('div');
+        showCardButtons.classList.add('show-card-buttons');
+
+        const likeButton = document.createElement('div');
+        likeButton.classList.add('like-button');
+
+        const likeIcon = document.createElement('img');
+        likeIcon.classList.add('like-icon');
+        likeIcon.src = '../media/pngwing.com.png';
+        likeIcon.alt = 'Like Icon';
+        likeButton.appendChild(likeIcon);
+
+        const commentsButton = document.createElement('button');
+        commentsButton.classList.add('comments-button');
+        commentsButton.innerText = 'Comments';
+
+        const imageContainer = document.createElement('div');
+        imageContainer.classList.add('image-container');
+
+        const image = document.createElement('img');
+        image.setAttribute('src', showData.image.medium);
+        image.setAttribute('alt', showData.name);
+
+        const buttonContainer = document.createElement('div');
+        buttonContainer.classList.add('button-container');
+
+        const reservationButton = document.createElement('button');
+        reservationButton.classList.add('reservation-button');
+        reservationButton.innerText = 'Reserve Now';
+
+        imageContainer.appendChild(image);
+
+        buttonContainer.appendChild(reservationButton);
+        buttonContainer.appendChild(commentsButton);
+
+        showCardHeader.appendChild(showCardTitle);
+        showCardHeader.appendChild(likeButton);
+
+        showCard.insertAdjacentHTML('beforeend', imageContainer.outerHTML + showCardHeader.outerHTML + buttonContainer.outerHTML);
+
+        display.appendChild(showCard);
+      });
 
       // Add event listeners to the lie buttons
       const lieButtons = document.querySelectorAll('.lie-button');
