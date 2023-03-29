bf048ee588584f865c21a5e7d91a37c4f1eb75b5
Bisaso Ronald <bronnie313@gmail.com>
2023-03-29 11:47:09 +0300

    commit my popup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+
+const closeBtn = document.getElementById('close-button');
+const openComments = document.getElementById('open-comments');
+const showMovie = document.getElementById('movie');
+
+openComments.addEventListener('click', () => {
+  showMovie.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+  showMovie.style.display = 'none';
+});
